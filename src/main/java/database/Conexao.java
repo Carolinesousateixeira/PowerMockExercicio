00f7572f8a3fae17fee1db3cfdf95cb3761c529a
Carolinesousateixeira <teixeirasousa.caroline@gmail.com>
2018-12-07 19:11:08 -0300

    Update Conexao.java

--- src/main/java/database/Conexao.java
+++ src/main/java/database/Conexao.java
@@ -1,18 +1,7 @@
 package database;
 
 public class Conexao {
-	private static Conexao instancia;
 	
-	private Conexao() {
-		
-	}
-	
-	public static Conexao getInstancia() {
-		if (instancia == null) {
-			instancia = new Conexao();
-		}
-		return instancia;
-	}
 	
 	public static boolean estaFechado() {
 		return false;
